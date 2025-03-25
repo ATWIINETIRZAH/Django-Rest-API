47ca19bc612bdab9f942272b5710e39c084dc318
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-03-25 04:36:19 +0300

    defined model in models.py

--- myitems/backend/backend/inventory/models.py
+++ myitems/backend/backend/inventory/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Item(models.Model):
+    user = models. ForeignKey(User, on_delete=models.CASCADE)
+    name = models. CharField(max_length=100)
+    model = models. CharField(max_length=100)
+    cost = models. DecimalField(max_digits=10, decimal_places=2)
+
+def _str_(self):
+    return f"{self.name} - {self.model}"
+
+
