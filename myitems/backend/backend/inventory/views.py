c486a47d2abf063dbe50b0840e15c96ada7982c3
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-03-25 04:45:41 +0300

    created different views

--- myitems/backend/backend/inventory/views.py
+++ myitems/backend/backend/inventory/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
+from django. contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import Item
+from .serializer import ItemSerializer
+
+class RegisterView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'User already exists'}, status=400)
+        User.objects.create_user(username=username, password=password)
+        return Response({'message': 'User created'}, status=201)
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+
+
+        return Response({'error': 'Invalid credentials'}, status=401)
+
+class ItemListCreateView(generics.ListCreateAPIView):
+    serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
+
+def get_queryset(self):
+    return Item.objects.filter(user=self.request.user)
+
+def perform_create(self, serializer):
+    serializer.save(user=self.request.user)
+
+class ItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
+
+def get_queryset(self):
+    return Item.objects.filter(user=self.request.user)
