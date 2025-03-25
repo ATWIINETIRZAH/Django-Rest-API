6b424e2f4b19513d9c569cc11c9839e7c286d120
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-03-25 04:53:43 +0300

    fixed rest_framework name

--- myitems/backend/backend/backend/settings.py
+++ myitems/backend/backend/backend/settings.py
@@ -78,7 +78,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
 'DEFAULT_AUTHENTICATION_CLASSES' : (
-'rest_framework_simplejwt. authentication. JWTAuthentication',
+# 'rest_framework_simplejwt. authentication. JWTAuthentication',
+'rest_framework_simplejwt.authentication.JWTAuthentication',
+
 
 )
 
